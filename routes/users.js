878dbe00ab36f8959208147d76ad62a40f881689
Павел Завадский <zavadskiy.pavel2002@outlook.com>
2021-09-11 21:40:38 +0300

    add docker-compose add passport

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
+const {route} = require("express/lib/router");
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send(req);
+});
+
+router.post('/', (req, res, next) => {
+
 });
 
 module.exports = router;
